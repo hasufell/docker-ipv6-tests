bd326e17cbd513ff21b42cad5c19c9421e538538
hasufell <hasufell@hasufell.de>
2015-09-07 18:04:13 +0200

    Fix docker container names

--- bin/stop-ipv6-mail-container.sh
+++ bin/stop-ipv6-mail-container.sh
@@ -5,9 +5,9 @@ die() {
         exit 1
 }
 
-docker stop hasufell/nginx-proxy
-docker rm hasufell/nginx-proxy
+docker stop nginx-proxy
+docker rm nginx-proxy
 
-docker stop hasufell/docker-postfix-dovecot
-docker rm hasufell/docker-postfix-dovecot
+docker stop dockermail
+docker rm dockermail
 
