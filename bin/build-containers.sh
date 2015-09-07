9976d2c208d7dfaa3b9bea8f535f25d984277cad
hasufell <hasufell@hasufell.de>
2015-09-07 15:39:27 +0200

    Simplify building of containers

--- bin/build-containers.sh
+++ bin/build-containers.sh
@@ -1,13 +1,5 @@
 #!/bin/bash
 
-pushd dockermail/core > /dev/null
-docker build -t hasufell/docker-postfix-dovecot .
-popd > /dev/null
-
-pushd nginx-proxy > /dev/null
-docker build -t hasufell/nginx-proxy .
-popd > /dev/null
-
-pushd phantomjs > /dev/null
-docker build -t hasufell/phantomjs .
-popd > /dev/null
+docker build -t hasufell/docker-postfix-dovecot dockermail/core
+docker build -t hasufell/nginx-proxy nginx-proxy
+docker build -t hasufell/phantomjs phantomjs
